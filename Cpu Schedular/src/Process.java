ad4ac62f73d7ac3bf8057bd782d296fc6bcff49e
Body811 <bodyashraf500@gmail.com>
2023-12-11 10:55:08 +0200

    Update Process.java

--- Cpu Schedular/src/Process.java
+++ Cpu Schedular/src/Process.java
@@ -1,13 +1,25 @@
 public class Process {
     private String name;
-    private String Color;
+    private String color;
     private int arrivalTime;
     private int completionTime;
     private int burstTime;
     private int waitTime;
-
+    private int priority = 0;
+    private int AGFactor = 0;
     public String getName() {
         return name;
+    }
+
+    public Process(String name, String color, int arrivalTime, int completionTime, int burstTime, int waitTime, int priority, int AGFactor) {
+        this.name = name;
+        this.color = color;
+        this.arrivalTime = arrivalTime;
+        this.completionTime = completionTime;
+        this.burstTime = burstTime;
+        this.waitTime = waitTime;
+        this.priority = priority;
+        this.AGFactor = AGFactor;
     }
 
     public void setName(String name) {
@@ -15,11 +27,11 @@ public class Process {
     }
 
     public String getColor() {
-        return Color;
+        return color;
     }
 
     public void setColor(String color) {
-        Color = color;
+        color = color;
     }
 
     public int getArrivalTime() {
@@ -52,5 +64,21 @@ public class Process {
 
     public void setWaitTime(int waitTime) {
         this.waitTime = waitTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getAGFactor() {
+        return AGFactor;
+    }
+
+    public void setAGFactor(int AGFactor) {
+        this.AGFactor = AGFactor;
     }
 }
