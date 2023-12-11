740c382c3651c91a9b4b246ecbb59c45b7b8b66f
Body811 <bodyashraf500@gmail.com>
2023-12-11 10:44:53 +0200

    added process class

---
+++ Cpu Schedular/src/Process.java
@@ -0,0 +1,56 @@
+public class Process {
+    private String name;
+    private String Color;
+    private int arrivalTime;
+    private int completionTime;
+    private int burstTime;
+    private int waitTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setCompletionTime(int completionTime) {
+        this.completionTime = completionTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public int getWaitTime() {
+        return waitTime;
+    }
+
+    public void setWaitTime(int waitTime) {
+        this.waitTime = waitTime;
+    }
+}
